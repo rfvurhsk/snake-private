2b46be80af2beeb801ed1da02a6a22cfc31e70c6
rfvurhsk <2086754496@qq.com>
2025-07-05 06:04:58 +0800

    Update launcher_pro.lua

--- launcher_pro.lua
+++ launcher_pro.lua
@@ -1,25 +1,24 @@
-local _Gm = game
-local _Pid = _Gm.PlaceId
-
-local _r = ("ht".."tps://ra".."w.".."github".."user".."content.com/"):gsub("a","")
-local _repo = "rfvurhsk"
-local _branch = "snake-private"
-local _path = _r .. _repo .. "/" .. _branch .. "/main/"
-
-local function _x(f)
-    loadstring(_Gm:HttpGet(_path .. f))()
+local a = game
+local b = a.PlaceId
+local f = "https://raw.githubusercontent.com/rfvurhsk/snake-private/main/"
+local function g(file)
+    local s, r = pcall(function()
+        return loadstring(game:HttpGet(f .. file))()
+    end)
+    if not s then warn("❌ 脚本加载失败：" .. tostring(r)) end
 end
 
-local _id1 = 5e9 + 7497348201  -- 12497348201
-local _id2 = 2e10 - 7512645653 -- 12497354347
-local _id3 = 4e9 + 8497360072  -- 12497360072
+-- 混淆后的 PlaceId
+local id1 = 10000000000 + 2497348201 -- FNAF 一代 = 12497348201
+local id2 = 10000000000 + 2497354347 -- FNAF 二代 = 12497354347
+local id3 = 10000000000 + 2497360072 -- FNAF 三代 = 12497360072
 
-if _Pid == _id1 then
-    _x("fnaf1.lua")
-elseif _Pid == _id2 then
-    _x("fnaf2.lua")
-elseif _Pid == _id3 then
-    _x("fnaf3.lua")
+if b == id1 then
+    g("fnaf1.lua")
+elseif b == id2 then
+    g("fnaf2.lua")
+elseif b == id3 then
+    g("fnaf3.lua")
 else
-    (warn or print)("❌ 当前游戏不是支持的 FNAF 一/二/三代")
+    warn("❌ 当前游戏不是支持的 FNAF 一/二/三代")
 end
