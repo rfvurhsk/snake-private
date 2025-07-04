0035a97642e73330f250ff121d3e799475934eea
rfvurhsk <2086754496@qq.com>
2025-07-04 16:20:31 +0800

    Update launcher_pro.lua

--- launcher_pro.lua
+++ launcher_pro.lua
@@ -1,21 +1,26 @@
-local a = game
-local b = a.PlaceId
-local c = getfenv and getfenv() or {}
+local _Gm = game
+local _Pid = _Gm.PlaceId
 
-local d = 10000000000 + 2497348201
-local e = 10000000000 + 2497354347
+local _r = ("ht".."tps://ra".."w.".."github".."user".."content.com/"):gsub("a","") -- 字符混淆拼接
+local _repo = "rfvurhsk"
+local _branch = "snake-private"
+local _path = _r .. _repo .. "/" .. _branch .. "/main/"
 
-local f = ("https://" .. "raw." .. "github" .. "usercontent.com/")
-    .. "rfvurhsk/snake-private/main/"
-
-local function g(_, file)
-    loadstring(game:HttpGet(f .. file))()
+local function _x(f)
+    loadstring(_Gm:HttpGet(_path .. f))()
 end
 
-if b == d then
-    g(d, "fnaf1.lua")
-elseif b == e then
-    g(e, "fnaf2.lua")
+-- 用表达式伪装的 PlaceId
+local _id1 = 5e9 + 7497348201  -- FNAF1
+local _id2 = 2e10 - 7512645653 -- FNAF2
+local _id3 = 4*10^9 + 8497360072  -- FNAF3
+
+if _Pid == _id1 then
+    _x("fnaf1.lua")
+elseif _Pid == _id2 then
+    _x("fnaf2.lua")
+elseif _Pid == _id3 then
+    _x("fnaf3.lua")
 else
-    (warn or print)("❌ 当前游戏不是支持的 FNAF 一代或二代")
+    (warn or print)("❌ 当前游戏不是支持的 FNAF 一/二/三代")
 end
