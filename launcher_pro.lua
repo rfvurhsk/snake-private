d95b19f5bd1e479287cc9a4b14564903523223a1
rfvurhsk <2086754496@qq.com>
2025-07-05 09:18:37 +0800

    Update launcher_pro.lua

--- launcher_pro.lua
+++ launcher_pro.lua
@@ -1,22 +1,24 @@
 local a, b, c = game, game.PlaceId, loadstring
-local f = "https://raw.githubusercontent.com/rfvurhsk/snake-private/main/"
+local d = "https://raw.githubusercontent.com/"
+local e = "rfvurhsk/snake-private/main/"
+local f = d .. e
 
-local function g(file)
+local function h(i)
     local s, r = pcall(function()
-        return c(game:HttpGet(f .. file))()
+        return c(game:HttpGet(f .. i))()
     end)
     if not s then warn("❌ 脚本加载失败：" .. tostring(r)) end
 end
 
 local map = {
-    [tonumber("0x2E35F339")] = "fnaf1.lua",
-    [2^34 - 112502089]       = "fnaf2.lua",
-    [0x2E35F4B8 + 4754]      = "fnaf3.lua",
-    [0x2E35F594 + 20262]     = "fnaf4.lua"
+    [0x2E35F339] = "fnaf1.lua",
+    [(2^34) - (1 << 27) - 358889] = "fnaf2.lua",
+    [((0x2E35F000 + 0x00000B94) + 4754)] = "fnaf3.lua",
+    [(2^30 * 2) + 4194304 + 639560] = "fnaf4.lua"
 }
 
 if map[b] then
-    g(map[b])
+    h(map[b])
 else
     warn("❌ 当前游戏不是支持的 FNAF 一/二/三/四代")
 end
