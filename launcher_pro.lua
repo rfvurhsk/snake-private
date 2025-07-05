f38860402211ceaffa7b337fedeed9dce40ace19
rfvurhsk <2086754496@qq.com>
2025-07-05 09:02:23 +0800

    Update launcher_pro.lua

--- launcher_pro.lua
+++ launcher_pro.lua
@@ -1,24 +1,24 @@
-local a = game
-local b = a.PlaceId
-local f = "https://raw.githubusercontent.com/rfvurhsk/snake-private/main/"
-local function g(file)
+local a, b, c = game, game.PlaceId, loadstring
+local d = "https://raw.githubusercontent.com/"
+local e = "rfvurhsk/snake-private/main/"
+local f = d .. e
+
+local function h(i)
     local s, r = pcall(function()
-        return loadstring(game:HttpGet(f .. file))()
+        return c(game:HttpGet(f .. i))()
     end)
     if not s then warn("❌ 脚本加载失败：" .. tostring(r)) end
 end
 
--- 混淆后的 PlaceId
-local id1 = 10000000000 + 2497348201 -- FNAF 一代 = 12497348201
-local id2 = 10000000000 + 2497354347 -- FNAF 二代 = 12497354347
-local id3 = 10000000000 + 2497360072 -- FNAF 三代 = 12497360072
+local map = {
+    [tonumber("0x2E35F339")] = "fnaf1.lua",
+    [2^34 - 112502089] = "fnaf2.lua",
+    [0x2E35F4B8] + 4754 = "fnaf3.lua",
+    [12490000000 + 7365956] = "fnaf4.lua"
+}
 
-if b == id1 then
-    g("fnaf1.lua")
-elseif b == id2 then
-    g("fnaf2.lua")
-elseif b == id3 then
-    g("fnaf3.lua")
+if map[b] then
+    h(map[b])
 else
-    warn("❌ 当前游戏不是支持的 FNAF 一/二/三代")
+    warn("❌ 当前游戏不是支持的 FNAF 一/二/三/四代")
 end
