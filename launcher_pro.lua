af14747de1426d86b4183558309a0f6689b7d167
rfvurhsk <2086754496@qq.com>
2025-07-05 09:29:18 +0800

    Update launcher_pro.lua

--- launcher_pro.lua
+++ launcher_pro.lua
@@ -8,16 +8,24 @@ local baseUrl = "https://raw.githubusercontent.com/rfvurhsk/snake-private/main/"
 
 local function loadScript(file)
     local success, result = pcall(function()
-        return loadFunc(game:HttpGet(baseUrl .. file))()
+        local scriptText = game:HttpGet(baseUrl .. file)
+        local f = loadFunc(scriptText)
+        if f then
+            f()
+        else
+            warn("❌ 加载的脚本无法运行")
+        end
     end)
-    if not success then warn("❌ 脚本加载失败：" .. tostring(result)) end
+    if not success then
+        warn("❌ 脚本加载失败：" .. tostring(result))
+    end
 end
 
 local map = {
-    [0x2E35F339] = "fnaf1.lua",
-    [(2^34) - (1 << 27) - 358889] = "fnaf2.lua",
-    [(0x2E35F000 + 0x00000B94 + 4754)] = "fnaf3.lua",
-    [(2^30 * 2) + 4194304 + 639560] = "fnaf4.lua"
+    [12497348201] = "fnaf1.lua",
+    [12497354347] = "fnaf2.lua",
+    [12497360072] = "fnaf3.lua",
+    [12497365956] = "fnaf4.lua"
 }
 
 if map[placeId] then
